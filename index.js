91c4307e89832ea4eec4442f75c621f055fbc8d4
Sarah Williams <92603276+Sarahlophus@users.noreply.github.com>
2022-03-13 17:11:10 -0500

    added functions to add new role, add new department

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function init() {
   inquirer
     .prompt([
       {
-        //questions here//
+        //initial user navigation questions here//
         name: "questions",
         type: "list",
         message: "What would you like to do?",
@@ -41,6 +41,14 @@ function init() {
           {
             name: "Add a new employee",
             value: "addEmp",
+          },
+          {
+            name: "Add a new job role",
+            value: "addRole",
+          },
+          {
+            name: "Add a new department",
+            value: "addDept",
           },
           {
             name: "Quit",
@@ -65,6 +73,12 @@ function init() {
       } else if (answers.questions === "addEmp") {
         // call addEmployee function
         addEmployee();
+      } else if (answers.questions === "addRole") {
+        // call addRole function
+        addRole();
+      } else if (answers.questions === "addDept") {
+        // call addDept function
+        addDept();
       }
     });
 }
@@ -102,14 +116,11 @@ function viewDepartments() {
   });
 }
 
-// different functions for each user choice - choose your own adventure! (do these last, clear viewAllEmployees first)
+// different functions for each user choice - choose your own adventure!
 
-// function addRole
-
-// function addDepartment
-
-// funtion addEmployee
+// funtion to add new employee to teamtracer_db(employees table)
 function addEmployee() {
+  // inquirer prompt to ask for new employee's first, last name
   inquirer
     .prompt([
       {
@@ -124,13 +135,14 @@ function addEmployee() {
       },
     ])
 
-    // promise
+    // promise to resolve linking to roles table
     .then((answers) => {
       db.query("SELECT * FROM roles", function (err, results) {
         const roles = results.map(({ id, title }) => ({
           name: title,
           value: id,
         }));
+        // inquirer prompt to ask for new employee's role
         inquirer
           .prompt({
             type: "list",
@@ -138,12 +150,14 @@ function addEmployee() {
             message: "What is the employee's role?",
             choices: roles,
           })
+          // promise to resolve assigning employee to manager
           .then((role) => {
             db.query("SELECT * FROM employees WHERE manager_id is null", function (err, results) {
               const managers = results.map(({ id, first_name }) => ({
                 name: first_name,
                 value: id,
               }));
+              // inquirer prompt to ask who manager is
               inquirer
                 .prompt({
                   type: "list",
@@ -151,9 +165,11 @@ function addEmployee() {
                   message: "What is their manager's name?",
                   choices: managers,
                 })
+                // promise to insert new employee's info into employee table
                 .then((manager) => {
                   // inserts new employee data into employees table on SQL
                   db.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answers.firstName, answers.lastName, role.id, manager.id]);
+                  console.log(`${answers.firstName} ${answers.lastName} has been added to your Employee List!`);
                   init();
                 });
             });
@@ -161,9 +177,70 @@ function addEmployee() {
       });
     });
 }
-// inquirer prompt to ask for first, last name
-//.then query roles table
-// inquirer prompt for choosing role
-//.then prompt to ask who manager is
 
+// function to add new role to teamtracer_db(roles table)
+function addRole() {
+  // inquirer promp to as for new role's title, salary,
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "Enter the job title of this new role",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Enter the salary of this new role",
+      },
+    ])
+
+    // promise to resolve linking to departments table
+    .then((answers) => {
+      db.query("SELECT * FROM departments", function (err, results) {
+        const depts = results.map(({ id, dept_name }) => ({
+          name: dept_name,
+          value: id,
+        }));
+        // inquirer prompt to ask for new role's department
+        inquirer
+          .prompt({
+            type: "list",
+            name: "id",
+            message: "Select the department for this new role",
+            choices: depts,
+          })
+          // promise to insert new role info into roles table
+          .then((department) => {
+            // inserts new role data into roles table on SQL
+            db.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", [answers.title, answers.salary, department.id]);
+            console.log(`${answers.title} has been added to your Job Roles!`);
+            init();
+          });
+      });
+    });
+}
+
+// function addDepartment
+function addDept() {
+  // inquirer promp to ask for new department's name,
+  inquirer
+    .prompt([
+      {
+        name: "deptName",
+        type: "input",
+        message: "Enter the name of this new department",
+      },
+    ])
+
+    // promise to insert new department info into department table
+    .then((answers) => {
+      // inserts new department data into roles table on SQL
+      db.query("INSERT INTO departments (dept_name) VALUES (?)", [answers.deptName]);
+      console.log(`${answers.deptName} has been added to your Departments!`);
+      init();
+    });
+}
+
+// start Inquirer program
 init();
